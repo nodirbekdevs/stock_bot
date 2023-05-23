51a313e1d1e0975f63618a1b7c03dcc83790c76c
nodirdevs <nodirbekdevs@mail.ru>
2023-05-23 15:01:39 +0500

    changed time_out and requirements.txt

--- src/loader.py
+++ src/loader.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.types import ParseMode
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from src.helpers.settings import TOKEN
+from src.helpers.settings import TOKEN, TIME_OUT
 
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML, timeout=TIME_OUT)
 dp = Dispatcher(bot, storage=MemoryStorage())
